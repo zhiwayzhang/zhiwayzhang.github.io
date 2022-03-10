eb687ce69457f048737712e0d657489d408d8413
EZ4ZZW <zhiwayzhang@outlook.com>
2022-03-10 10:32:48 +0800

    hugo

---
+++ post/test.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<set>
+#include<vector>
+#include<queue>
+const int MAXN = 1e5;
+const int MOD = 1e9+7;
+typedef long long ll;
+using namespace std;
+
+
+
+int main() {
+
+
+    return 0;
+}
